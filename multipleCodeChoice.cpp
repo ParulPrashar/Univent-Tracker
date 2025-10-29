2218b9300fc5e28f459ece1f26f62c0344a5f6bb
ParulPrashar <parulprashar018@gmail.com>
2025-10-29 21:34:46 +0530

    Added main code for multiple searches

---
+++ multipleCodeChoice.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream> 
+#include <algorithm>
+#include <cctype>
+#include <string>
+#include "multipleCodeChoice.h"
+
+using namespace std;
+
+//Commonly used functions
+string toLowerCase(string var){
+                transform(var.begin(), var.end(), var.begin(), [](unsigned char c){
+                    return tolower(c); });
+                    return var;
+                }
+
+//search functions
+void search_name() {
+    string id, name, date, venue;
+    string search_name;
+    string line; 
+    bool found = false;
+
+    cout << " Enter event name to search: ";
+    getline(cin, search_name);
+
+    search_name=toLowerCase(search_name);
+
+    ifstream file;
+    file.open("events.txt");
+
+    if (file.is_open()) {
+        
+       
+        while (getline(file, line)) {
+            
+            stringstream ss(line);
+
+            getline(ss, id, '|');
+            getline(ss, name, '|');
+            getline(ss, date, '|');
+            getline(ss, venue, '|'); 
+
+            string temp_name=name;
+            temp_name=toLowerCase(temp_name);
+            
+            if (temp_name == search_name) {
+                cout << " Event found!" << endl;
+                cout << " Event ID: " << id << endl;
+                cout << " Event Name: " << name << endl;
+                cout << " Event Date: " << date << endl;
+                cout << " Venue:"  << venue << endl;
+                found =true ;
+                break;
+            }
+        }
+        file.close();
+
+        if (found == false) {
+            cout << " Event not found! " << endl;
+        }
+    } else {
+         cerr<<" Error: Could not open file! "<<endl;
+    }
+}
+
+void search_venuename(){
+    string id, name, date, venue;
+    string search_venuename;
+    string line; 
+    bool found = false;
+
+    cout << " Enter venue name to search : ";
+    getline(cin, search_venuename);
+    search_venuename=toLowerCase(search_venuename);
+
+    ifstream file;
+    file.open("events.txt");
+
+    if (file.is_open()) {
+        
+       
+        while (getline(file, line)) {
+            
+            stringstream ss(line);
+
+         
+            getline(ss, id, '|');
+            getline(ss, name, '|');
+            getline(ss, date, '|');
+            getline(ss, venue, '|'); 
+
+            string temp_venue=venue;
+            temp_venue=toLowerCase(temp_venue);
+            
+            if (temp_venue == search_venuename) {
+                found =true ;
+                cout << " Events found on this venue ! " << endl;
+                cout << " Event ID : " << id << endl;
+                cout << " Event Name : " << name << endl;
+                cout << " Event Date : " << date << endl;
+                cout << " Venue : "  << venue << endl;
+                
+            }
+        }
+        file.close();
+
+        if (found == false) {
+            cout << " Event not found! " << endl;
+        }
+    } else {
+        cerr<<" Error: Could not open file! "<<endl;
+    }
+}
+
+
+
+// int main() {
+//     cout << "\n--- Event Search Menu ---" << endl;
+//     cout << "1 -->   Search by Event Name" << endl;
+//     cout << "2 -->  Search by Venue Name" << endl;
+    
+//     int input_from_user;
+//     cin>>input_from_user;
+//     cin.ignore();
+//     if (input_from_user==1){
+//         search_name();
+
+//     }
+//     else if (input_from_user==2)
+//     {
+//         search_venuename();
+//     }
+//     else{
+//         cout<<"invalid input";
+//     }
+    
+//     return 0;
+// }
