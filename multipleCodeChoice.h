9ce3d952ed9fb3d0c2d7a4eb68e53e503410189e
ParulPrashar <parulprashar018@gmail.com>
2025-10-29 21:33:47 +0530

    Created a new file for menu options
    We are integration 3 types of searches ( by event name, by venue and by date ). Likewise 3 types of Sorting. Hence created a new header file to store the date. Additionally to prevent code repetition for lower case functions ( to check equality of search and conditions in sort ), we added a new function toLowerCase() to deal with this.

---
+++ multipleCodeChoice.h
@@ -0,0 +1,14 @@
+#ifndef MultipleChoice_H
+#define MultipleChoice_H
+#include<iostream>
+#include<string>
+using namespace std;
+
+//commonly used functions
+string toLowerCase(string);
+
+//Different Search Codes
+void search_name();
+void search_venuename();
+
+#endif
