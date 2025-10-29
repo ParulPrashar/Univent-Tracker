37b7d33947cd6eb1a1e19a140e9b5fed99b1827b
ParulPrashar <parulprashar018@gmail.com>
2025-10-29 21:27:11 +0530

    Updated progress of completed tasks

--- Functions.h
+++ Functions.h
@@ -4,11 +4,11 @@
 #include<string>
 using namespace std;
 
-int insert_event();
+int insert_event();//done
 
 int delete_event();
 
-int search_event();
+int search_event();//done 2 types- 3rd left (date)
 
 int sort_event();
 
